ada8f170f27e54971483a8329023edca95792802
jossanrui5 <jossanrui5@alum.us.es>
2020-04-23 15:01:30 +0200

    Commit E2E de airline, client, plane y runway terminados

--- src/test/java/acmevolar/web/e2e/ClientControllerTestsE2E.java
+++ src/test/java/acmevolar/web/e2e/ClientControllerTestsE2E.java
@@ -1,11 +1,5 @@
-package acmevolar.web.e2e;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+package acmevolar.web.e2e;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,78 +9,64 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(
-  webEnvironment=SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 public class ClientControllerTestsE2E {
 
-	private static final int TEST_CLIENT_ID = 1;
+	private static final int	TEST_CLIENT_ID	= 1;
 
 	@Autowired
-	private MockMvc mockMvc;
-	
+	private MockMvc				mockMvc;
 
-	@WithMockUser(value = "spring")
+
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
 	@Test
 	void testShowClientList() throws Exception {
-		this.mockMvc.perform(get("/clients")).andExpect(status().isOk()).andExpect(model().attributeExists("clients"))
-				.andExpect(view().name("clients/clientsList"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/clients")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("clients")).andExpect(MockMvcResultMatchers.view().name("clients/clientsList"));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
 	@Test
 	void testShowClient() throws Exception {
-		this.mockMvc.perform(get("/clients/{clientId}", ClientControllerTestsE2E.TEST_CLIENT_ID))
-				.andExpect(status().isOk()).andExpect(model().attributeExists("client"))
-				.andExpect(view().name("clients/clientDetails"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/clients/{clientId}", ClientControllerTestsE2E.TEST_CLIENT_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("client"))
+			.andExpect(MockMvcResultMatchers.view().name("clients/clientDetails"));
 	}
 
-	@WithMockUser(value = "spring")
 	@Test
 	void testInitCreationForm() throws Exception {
-		this.mockMvc.perform(get("/clients/new"))
-			.andExpect(status().isOk())
-			.andExpect(view().name("clients/createClientForm"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/clients/new")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("clients/createClientForm"));
 	}
 
-	@WithMockUser(value = "spring")
 	@ParameterizedTest
 	@CsvSource({
-		"Pepito Pinotes, 123456789X, 1997/10/10, 987654321, pepitopinotes@alum.us.es",
-		"JU4N N06UER01, 666999666T, 1925/11/10, 634964979, jnogtir@alum.us.es",
-		"DIOS DANNY, 4458X, 1997/10/10, 4458, diosDanny4458@dany.god",
+		"Pepito Pinotes, 123456789X, 1997/10/10, 987654321, pepitopinotes@alum.us.es", "JU4N N06UER01, 666999666T, 1925/11/10, 634964979, jnogtir@alum.us.es", "DIOS DANNY, 4458X, 1997/10/10, 4458, diosDanny4458@dany.god",
 	})
-	void testProcessCreationFormSuccess(String name, String identification, String birthDate, String phone, String email) throws Exception {
-		this.mockMvc.perform(post("/clients/new").with(csrf())
-				.param("name", name)
-				.param("identification", identification)
-				.param("birthDate", birthDate)
-				.param("phone", phone)
-				.param("email", email))
-				.andExpect(status().is3xxRedirection());
+	void testProcessCreationFormSuccess(final String name, final String identification, final String birthDate, final String phone, final String email) throws Exception {
+		this.mockMvc
+			.perform(MockMvcRequestBuilders.post("/clients/new").with(SecurityMockMvcRequestPostProcessors.csrf()).param("name", name).param("identification", identification).param("birthDate", birthDate).param("phone", phone).param("email", email))
+			.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
 	}
-	
-	@WithMockUser(value = "spring")
+
 	@ParameterizedTest
 	@CsvSource({
-		"Pepito Pinotes, 123456789X, 2050/10/10, 987654321, pepitopinotes@alum.us.es",
-		"JU4N N06UER01, 666999666T, 1925/11/10, notanumber, jnogtir@alum.us.es",
-		"DIOS DANNY, 4458X, 1997/10/10, 4458, diosDanny4458.dany.god",
+		"Pepito Pinotes, 123456789X, 2050/10/10, 987654321, pepitopinotes@alum.us.es", "JU4N N06UER01, 666999666T, 1925/11/10, notanumber, jnogtir@alum.us.es", "DIOS DANNY, 4458X, 1997/10/10, 4458, diosDanny4458.dany.god",
 	})
-	void testProcessCreateFormHasErrors(String name, String identification, String birthDate, String phone, String email) throws Exception {
-		this.mockMvc.perform(post("/clients/new").with(csrf())
-				.param("name", name)
-				.param("identification", identification)
-				.param("birthDate", birthDate)
-				.param("phone", phone)
-				.param("email", email))
-				.andExpect(model().attributeHasErrors("client"))
-				.andExpect(view().name("clients/createClientForm"));
-		
+	void testProcessCreateFormHasErrors(final String name, final String identification, final String birthDate, final String phone, final String email) throws Exception {
+		this.mockMvc
+			.perform(MockMvcRequestBuilders.post("/clients/new").with(SecurityMockMvcRequestPostProcessors.csrf()).param("name", name).param("identification", identification).param("birthDate", birthDate).param("phone", phone).param("email", email))
+			.andExpect(MockMvcResultMatchers.model().attributeHasErrors("client")).andExpect(MockMvcResultMatchers.view().name("clients/createClientForm"));
+
 	}
 
 }
--- src/test/java/acmevolar/web/e2e/RunwayControllerTestsE2E.java
+++ src/test/java/acmevolar/web/e2e/RunwayControllerTestsE2E.java
@@ -1,13 +1,6 @@
 
 package acmevolar.web.e2e;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,142 +9,137 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(
-  webEnvironment=SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 class RunwayControllerTestsE2E {
 
-	private static final int TEST_RUNWAY_ID1 = 1; // Departes from Sevilla
+	private static final int	TEST_RUNWAY_ID1		= 1; // Departes from Sevilla
 
-	private static final int TEST_AIRPORT_ID1 = 1;// Sevilla
+	private static final int	TEST_AIRPORT_ID1	= 1;// Sevilla
 
-	
 	@Autowired
-	private MockMvc mockMvc;
+	private MockMvc				mockMvc;
 
 
-	@WithMockUser(value = "airline1"/*, authorities = { "airline" }*/)
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
 	@Test
 	void testShowRunwayList() throws Exception {
-		this.mockMvc.perform(get("/airports/{airportId}/runways", TEST_AIRPORT_ID1)).andExpect(status().isOk())
-				.andExpect(model().attributeExists("runways", "airport")).andExpect(view().name("runways/runwayList"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways", RunwayControllerTestsE2E.TEST_AIRPORT_ID1)).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.model().attributeExists("runways", "airport")).andExpect(MockMvcResultMatchers.view().name("runways/runwayList"));
 	}
 
 	@Test
 	void testUnauthorizedShowRunwayList() throws Exception {
-		this.mockMvc.perform(get("/airports/{airportId}/runways", TEST_AIRPORT_ID1)).andExpect(status().is4xxClientError());
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways", RunwayControllerTestsE2E.TEST_AIRPORT_ID1)).andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 	}
-	
-	@WithMockUser(value = "airline1", authorities = { "airline" })
+
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
 	@Test
 	void testInitCreationForm() throws Exception {
-		this.mockMvc.perform(get("/airports/{airportId}/runways/new", TEST_AIRPORT_ID1)).andExpect(status().isOk())
-				.andExpect(model().attributeExists("runway", "airport"))
-				.andExpect(view().name("runways/createOrUpdateRunwaysForm"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways/new", RunwayControllerTestsE2E.TEST_AIRPORT_ID1)).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.model().attributeExists("runway", "airport")).andExpect(MockMvcResultMatchers.view().name("runways/createOrUpdateRunwaysForm"));
 	}
 
 	@Test
 	void testUnauthorizedInitCreationForm() throws Exception {
-		this.mockMvc.perform(get("/airports/{airportId}/runways/new", TEST_AIRPORT_ID1)).andExpect(status().is4xxClientError());
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways/new", RunwayControllerTestsE2E.TEST_AIRPORT_ID1)).andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 	}
-	
-	@WithMockUser(value = "airline1", authorities = { "airline" })
-	@Test
-	void testProcessCreationFormSuccess() throws Exception {
-		this.mockMvc
-				.perform(post("/airports/{airportId}/runways/new", TEST_AIRPORT_ID1).with(csrf())
-						.param("name", "Runway Example").param("runwayType", "landing"))
-				.andExpect(status().is3xxRedirection());
-	}
-	
-	@Test
-	void testUnauthorizedProcessCreationForm() throws Exception {
-		this.mockMvc
-				.perform(post("/airports/{airportId}/runways/new", TEST_AIRPORT_ID1).with(csrf())
-						.param("name", "Runway Example").param("runwayType", "landing"))
-				.andExpect(status().is4xxClientError());
-	}
-	
+
 	@WithMockUser(value = "airline1", authorities = {
 		"airline"
 	})
-@ParameterizedTest 
-@CsvSource({
-    "runway1, landing",
-    "runway2, take_off",
-    "runway3, landing",
-    "runway4, take_off",
-}) 
-void testProcessCreationFormSuccess(String name, String runwayType) throws Exception {    
-	this.mockMvc.perform(post("/airports/{airportId}/runways/new", TEST_AIRPORT_ID1).with(csrf())
-			.param("name", name)
-			.param("runwayType", runwayType))
-			.andExpect(status().is3xxRedirection());
-} 
-	
-	@WithMockUser(value = "airline1", authorities = { "airline" })
 	@Test
-	void testProcessCreationFormHasErrors() throws Exception {
-		this.mockMvc
-				.perform(post("/airports/{airportId}/runways/new", TEST_AIRPORT_ID1).with(csrf())
-						.param("name", "")
-						.param("runwayType", "take_off"))
-				.andExpect(model().attributeHasErrors("runway")).andExpect(view().name("runways/createOrUpdateRunwaysForm"));
+	void testProcessCreationFormSuccess() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/airports/{airportId}/runways/new", RunwayControllerTestsE2E.TEST_AIRPORT_ID1).with(SecurityMockMvcRequestPostProcessors.csrf()).param("name", "Runway Example").param("runwayType", "landing"))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 	}
 
-	@WithMockUser(value = "airline1", authorities = { "airline" })
+	@Test
+	void testUnauthorizedProcessCreationForm() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/airports/{airportId}/runways/new", RunwayControllerTestsE2E.TEST_AIRPORT_ID1).with(SecurityMockMvcRequestPostProcessors.csrf()).param("name", "Runway Example").param("runwayType", "landing"))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+	}
+
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
+	@ParameterizedTest
+	@CsvSource({
+		"runway1, landing", "runway2, take_off", "runway3, landing", "runway4, take_off",
+	})
+	void testProcessCreationFormSuccess(final String name, final String runwayType) throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/airports/{airportId}/runways/new", RunwayControllerTestsE2E.TEST_AIRPORT_ID1).with(SecurityMockMvcRequestPostProcessors.csrf()).param("name", name).param("runwayType", runwayType))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+	}
+
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
+	@Test
+	void testProcessCreationFormHasErrors() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/airports/{airportId}/runways/new", RunwayControllerTestsE2E.TEST_AIRPORT_ID1).with(SecurityMockMvcRequestPostProcessors.csrf()).param("name", "").param("runwayType", "take_off"))
+			.andExpect(MockMvcResultMatchers.model().attributeHasErrors("runway")).andExpect(MockMvcResultMatchers.view().name("runways/createOrUpdateRunwaysForm"));
+	}
+
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
 	@Test
 	void testInitUpdateForm() throws Exception {
-		mockMvc.perform(get("/airports/{airportId}/runways/{runwayId}/edit", TEST_AIRPORT_ID1, TEST_RUNWAY_ID1))
-				.andExpect(status().isOk()).andExpect(model().attributeExists("runway", "runwayTypes"))
-				.andExpect(view().name("runways/createOrUpdateRunwaysForm"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways/{runwayId}/edit", RunwayControllerTestsE2E.TEST_AIRPORT_ID1, RunwayControllerTestsE2E.TEST_RUNWAY_ID1)).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.model().attributeExists("runway", "runwayTypes")).andExpect(MockMvcResultMatchers.view().name("runways/createOrUpdateRunwaysForm"));
 	}
 
 	@Test
 	void testUnauthorizedInitUpdateForm() throws Exception {
-		mockMvc.perform(get("/airports/{airportId}/runways/{runwayId}/edit", TEST_AIRPORT_ID1, TEST_RUNWAY_ID1))
-				.andExpect(status().is4xxClientError());
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways/{runwayId}/edit", RunwayControllerTestsE2E.TEST_AIRPORT_ID1, RunwayControllerTestsE2E.TEST_RUNWAY_ID1)).andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 	}
-	
-	@WithMockUser(value = "airline1", authorities = { "airline" })
+
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
 	@Test
 	void testProcessUpdateFormSuccess() throws Exception {
-		mockMvc.perform(post("/airports/{airportId}/runways/{runwayId}/edit", TEST_AIRPORT_ID1, TEST_RUNWAY_ID1)
-				.with(csrf()).param("name", "Runway Example").param("runwayType", "landing"))
-				.andExpect(status().is3xxRedirection())
-				.andExpect(view().name("redirect:/airports/{airportId}/runways"));
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/airports/{airportId}/runways/{runwayId}/edit", RunwayControllerTestsE2E.TEST_AIRPORT_ID1, RunwayControllerTestsE2E.TEST_RUNWAY_ID1).with(SecurityMockMvcRequestPostProcessors.csrf())
+			.param("name", "Runway Example").param("runwayType", "landing")).andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
 	}
-	
+
 	@Test
 	void testUnauthorizedProcessUpdateForm() throws Exception {
-		mockMvc.perform(post("/airports/{airportId}/runways/{runwayId}/edit", TEST_AIRPORT_ID1, TEST_RUNWAY_ID1)
-				.with(csrf()))
-				.andExpect(status().is4xxClientError());
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/airports/{airportId}/runways/{runwayId}/edit", RunwayControllerTestsE2E.TEST_AIRPORT_ID1, RunwayControllerTestsE2E.TEST_RUNWAY_ID1).with(SecurityMockMvcRequestPostProcessors.csrf()))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 	}
-	
-	@WithMockUser(value = "airline1", authorities = { "airline" })
+
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
 	@Test
 	void testDeleteSuccess() throws Exception {
-		mockMvc.perform(get("/airports/{airportId}/runways/{runwayId}/delete", TEST_AIRPORT_ID1, TEST_RUNWAY_ID1))
-				.andExpect(status().is3xxRedirection())
-				.andExpect(view().name("redirect:/airports/{airportId}/runways"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways/{runwayId}/delete", RunwayControllerTestsE2E.TEST_AIRPORT_ID1, RunwayControllerTestsE2E.TEST_RUNWAY_ID1)).andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
 	}
-	
-	@WithMockUser(value = "airline1", authorities = { "airline" })
+
+	@WithMockUser(value = "airline1", authorities = {
+		"airline"
+	})
 	@Test
 	void testDeleteHasErrors() throws Exception {
-		mockMvc.perform(get("/airports/{airportId}/runways/{runwayId}/delete", TEST_AIRPORT_ID1, 2))
-				.andExpect(view().name("exception"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways/{runwayId}/delete", RunwayControllerTestsE2E.TEST_AIRPORT_ID1, 2)).andExpect(MockMvcResultMatchers.view().name("exception"));
 	}
-	
+
 	@Test
 	void testDeleteUnauthorized() throws Exception {
-		mockMvc.perform(get("/airports/{airportId}/runways/{runwayId}/delete", TEST_AIRPORT_ID1, TEST_RUNWAY_ID1))
-				.andExpect(status().is4xxClientError());
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/airports/{airportId}/runways/{runwayId}/delete", RunwayControllerTestsE2E.TEST_AIRPORT_ID1, RunwayControllerTestsE2E.TEST_RUNWAY_ID1)).andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 	}
 
 }

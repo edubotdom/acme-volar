e1d6e094a7630d0eac06d5cd53f82888243afd57
jossanrui5 <jossanrui5@alum.us.es>
2020-05-27 13:42:56 +0200

    Commit profiling y optimizacion de airport y runway listo

---
+++ src/main/java/acmevolar/projections/AirportListAttributes.java
@@ -0,0 +1,15 @@
+
+package acmevolar.projections;
+
+public interface AirportListAttributes {
+
+	String getId();
+	String getName();
+	Integer getMaxNumberOfPlanes();
+	Integer getMaxNumberOfClients();
+	Double getLatitude();
+	Double getLongitude();
+	String getCode();
+	String getCity();
+
+}
